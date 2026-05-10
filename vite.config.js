c471c799f6fae0fca96b2ca2f51d81e7a6ab910f
msdsshopify-beep <msdsshopify@gmail.com>
2026-05-11 01:23:02 +0530

    final fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
   base: '/websitedemo/',
-  build: {
-    outDir: 'docs', // Build to the docs folder for GitHub pages
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        contact: resolve(__dirname, 'contact.html')
-      }
-    }
-  }
 })
